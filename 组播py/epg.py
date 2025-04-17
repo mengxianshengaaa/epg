d8a612fbaf23fa6d5b5ac659eb53699c29108400
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2025-04-17 09:15:41 +0800

    Update and rename 组播综合.py to epg.py

---
+++ 组播py/epg.py
@@ -0,0 +1,16 @@
+import requests
+
+def download_file(url, save_path):
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+url = 'https://epg.112114.xyz/pp.xml'
+save_path = 'epg.xml'
+download_file(url, save_path)
+
+        print(f"文件 {file} 不存在，跳过删除。")
+
+print("任务运行完毕，分类频道列表可查看文件夹内iptv_list.txt文件！")
