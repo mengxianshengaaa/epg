193ff76492d65359e4ecf7c677739d78de6581a4
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2025-04-22 20:37:41 +0800

    Update epg.py

--- epg.py
+++ epg.py
@@ -1,5 +1,4 @@
 import requests
-import subprocess
 import os
 
 urls = [
@@ -17,23 +16,6 @@ for url in urls:
             file_size = os.path.getsize(file_name)
             if file_size > 0:
                 print(f"{file_name} 下载成功，文件大小: {file_size} 字节。")
-                try:
-                    subprocess.run(['git', 'add', file_name], check=True)
-                    print(f"Git 添加 {file_name} 成功。")
-                except subprocess.CalledProcessError as e:
-                    print(f"Git 添加 {file_name} 失败: {e}")
-                    continue
-                try:
-                    subprocess.run(['git', 'commit', '-m', f'Add {file_name}'], check=True)
-                    print(f"Git 提交 {file_name} 成功。")
-                except subprocess.CalledProcessError as e:
-                    print(f"Git 提交 {file_name} 失败: {e}")
-                    continue
-                try:
-                    subprocess.run(['git', 'push'], check=True)
-                    print(f"Git 推送 {file_name} 成功。")
-                except subprocess.CalledProcessError as e:
-                    print(f"Git 推送 {file_name} 失败: {e}")
             else:
                 print(f"{file_name} 下载可能失败，文件大小为 0 字节。")
         else:
