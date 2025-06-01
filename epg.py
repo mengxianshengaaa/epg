74a0c33d6bf6b866630258670db4b99f1c0eebaa
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2025-06-01 13:42:20 +0800

    Update epg.py

--- epg.py
+++ epg.py
@@ -8,7 +8,7 @@ urls = [
     "https://epg.112114.xyz/pp.xml",
     "https://epg.pw/xmltv/epg_HK.xml",
     "https://epg.pw/xmltv/epg_TW.xml",
-    "https://epg.pw/xmltv/epg_CN.xml",	
+    "https://epg.pw/xmltv/epg_CN.xml"
 ]
 
 # 存储下载的文件路径
